aa20201aad0720b52a352347fea5963ab417a7b6
Luis Fernando <luis_rogu@outlook.com>
2023-09-12 23:02:43 -0600

    Factors problem solved

--- Factor/Factor/src/Main.java
+++ Factor/Factor/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        System.out.println("Factors of a number");
+        factors(32);
+    }
+    public static void factors(int factorialOrder) {
+        int varFactorial = 1;
+        int resFactorial = 1;
+
+        for (int i = 1; i <= factorialOrder; i++) {
+            if ((factorialOrder % i) == 0)
+            {
+                System.out.println("Factor numbers of " + factorialOrder + ": " + i);
+            }
+            else
+            {
+                /*
+                Do Nothing
+                 */
+            }
+        }
     }
 }
