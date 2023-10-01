04a44f2d44c8520149c631265808dd74ae2d9cfe
Luis Fernando <luis_rogu@outlook.com>
2023-10-01 21:27:16 +0200

    SuperClass & SubClass example

--- Polymorphism/Exercise/src/Main.java
+++ Polymorphism/Exercise/src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Vehicle l_classElectricFord = new HybridV("2023 Ford 4x4","FORD", "Raptor", 120, 8);
+
+        l_classElectricFord.vehicleStartEngine();
+        l_classElectricFord.vehicleRunEngine();
+        l_classElectricFord.vehicleDrive();
     }
 }
